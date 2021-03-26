965a95af6cc318b74c8987fa2c40c2c65c2e208f
olympiawoj <owojcik7@gmail.com>
2021-03-26 10:28:18 -0400

    Visualizing Messages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
+import useInterval from '@use-it/interval'
 import './App.css';
+
 
 const messages = [
   { text: 'How do I get better at React?' },
@@ -12,17 +14,38 @@ const messages = [
 ];
 
 export default function App() {
+  const [messageToShow, setMessageToShow] = useState(0) // 1st message to start
+
+  useInterval(()=>{
+    setMessageToShow(messageToShow => messageToShow + 1)
+  }, 2000)
+
   return (
     <div className="app">
       <div className="walkthrough">
         {messages.map((message, index) => {
-          return (
-            <div key={index} className="message">
-              {message.text}
-            </div>
-          );
+          //logic goes here
+                    
+          // are we supposed to show a typing indicator?
+          if(messageToShow + 1 === index){
+            return <div key="index">I am typing</div>
+          }
+          // are we supposed to show this message?
+          if(index > messageToShow) return <div key={index}/>
+          return <Message message={message} key={index}/>
+          
         })}
       </div>
     </div>
   );
+}
+
+function Message({message, index}){
+  return (
+    <div className="message">
+    <div className="avatar">🐸</div>
+    <div className="text">{message.text}</div>
+    <div className="avatar">🐙</div>
+  </div>
+  )
 }
